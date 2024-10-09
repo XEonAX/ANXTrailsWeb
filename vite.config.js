a71713175902503352070b6ece6291af1566aa8e
XEonAX <sv.xeon@gmail.com>
2024-10-10 00:11:46 +0530

    Add Bloom, Clicks, And Title and Favicon

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,22 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
       },
-      external: ["three", "@microsoft/signalr"],
+      external: [
+        "three",
+        "three/examples/jsm/postprocessing/EffectComposer.js",
+        "three/examples/jsm/postprocessing/RenderPass.js",
+        "three/examples/jsm/postprocessing/UnrealBloomPass.js",
+        "@microsoft/signalr",
+      ],
       output: {
         paths: {
           three: "https://cdn.jsdelivr.net/npm/three@0.169.0/+esm",
+          "three/examples/jsm/postprocessing/EffectComposer.js":
+            "https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/postprocessing/EffectComposer.js",
+          "three/examples/jsm/postprocessing/RenderPass.js":
+            "https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/postprocessing/RenderPass.js",
+          "three/examples/jsm/postprocessing/UnrealBloomPass.js":
+            "https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/postprocessing/UnrealBloomPass.js",
           "@microsoft/signalr":
             "https://cdn.jsdelivr.net/npm/@microsoft/signalr@8.0.7/+esm",
         },
